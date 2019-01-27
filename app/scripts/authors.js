4ba563bb960c2b4ea43869a5de67766db0910534
“Simaxa7” <simaxa7@mail.ru>
2019-01-27 13:40:08 +0300

    refactor: edited authors according to the eslint's rules

--- app/scripts/authors.js
+++ app/scripts/authors.js
@@ -1,22 +1,22 @@
 function tableSearch() {
-    const phrase = document.getElementById('search-text');
-    const table = document.getElementById('info-table');
-    const regPhrase = new RegExp(phrase.value, 'i');
-    let flag = false;
-    for (let i = 1; i < table.rows.length; i++) {
-        flag = false;
-        for (let j = table.rows[i].cells.length - 1; j >= 0; j--) {
-            flag = regPhrase.test(table.rows[i].cells[j].innerHTML);
-            if (flag) break;
-        }
-        if (flag) {
-            table.rows[i].style.display = "";
-        } else {
-            table.rows[i].style.display = "none";
-        }
+  const phrase = document.getElementById('search-text');
+  const table = document.getElementById('info-table');
+  const regPhrase = new RegExp(phrase.value, 'i');
+  let flag = false;
+  for (let i = 1; i < table.rows.length; i++) {
+    flag = false;
+    for (let j = table.rows[i].cells.length - 1; j >= 0; j--) {
+      flag = regPhrase.test(table.rows[i].cells[j].innerHTML);
+      if (flag) break;
     }
+    if (flag) {
+      table.rows[i].style.display = '';
+    } else {
+      table.rows[i].style.display = 'none';
+    }
+  }
 }
 
-document.querySelector('#search-text').addEventListener('keyup', (e) => {
-	tableSearch();
+document.querySelector('#search-text').addEventListener('keyup', () => {
+  tableSearch();
 });
