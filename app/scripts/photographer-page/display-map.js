4ef68bdbe6d8d2960a857e18988bd2e99ba09716
zm1try <zmitryarosh@gmail.com>
2019-01-27 13:58:31 +0300

    refactor: edit according to the eslint's rules

--- app/scripts/photographer-page/display-map.js
+++ app/scripts/photographer-page/display-map.js
@@ -4,7 +4,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   id: 'mapbox.streets',
-  accessToken: 'pk.eyJ1Ijoiem0xdHJ5IiwiYSI6ImNqcmRwa2c3eTBrZWk0OXBvdnNlZGZqbHAifQ.4--wklrVhxA5gT6y4QM5RQ'
+  accessToken: 'pk.eyJ1Ijoiem0xdHJ5IiwiYSI6ImNqcmRwa2c3eTBrZWk0OXBvdnNlZGZqbHAifQ.4--wklrVhxA5gT6y4QM5RQ',
 }).addTo(mymap);
 
 L.marker([53.9015, 27.5575]).addTo(mymap).bindPopup('A pretty CSS3 popup.<br> Easily customizable.').openPopup();
