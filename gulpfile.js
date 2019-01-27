43a329bcf62491f0829811bf43bf9377029d2f7d
Paval Miatlitski <paval.escoba@gmail.com>
2019-01-27 12:49:31 +0300

    fix: add eslint replace imagemin-pngquant to older version

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,76 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var wait = require('gulp-wait');
-var browserSync = require('browser-sync');
-var rename = require('gulp-rename');
-var del = require('del');
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
-var cache = require('gulp-cache');
-var autoprefixer = require('gulp-autoprefixer');
-var uncss = require('gulp-uncss');
-var plumber = require('gulp-plumber');
-var sourcemaps = require('gulp-sourcemaps');
+/* eslint-disable no-unused-vars */
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const wait = require('gulp-wait');
+const browserSync = require('browser-sync');
+// const rename = require('gulp-rename');
+const del = require('del');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+const cache = require('gulp-cache');
+const autoprefixer = require('gulp-autoprefixer');
+// const uncss = require('gulp-uncss');
+const plumber = require('gulp-plumber');
+const sourcemaps = require('gulp-sourcemaps');
+const eslint = require('gulp-eslint');
 
-gulp.task('sass', function () {
-  return gulp.src('app/scss/**/*.scss')
-    .pipe(wait(500))
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(autoprefixer(['last 3 versions', '> 1%', ], {
-      cascade: true
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
+gulp.task('sass', () => gulp.src('app/scss/**/*.scss')
+  .pipe(wait(500))
+  .pipe(plumber())
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(autoprefixer(['last 3 versions', '> 1%'], {
+    cascade: true,
+  }))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('app/css'))
+  .pipe(browserSync.reload({
+    stream: true,
+  })));
 
-gulp.task('browser-sync', function () {
+gulp.task('lint', () => gulp.src(['app/scripts/*.js', '!app/scripts/bootstrap.min.js', '!app/scripts/jquery.min.js', '!app/scripts/popper.min.js', '!node_modules', '!gulpfile.js'])
+  .pipe(eslint())
+  .pipe(eslint.format()));
+
+gulp.task('browser-sync', () => {
   browserSync({
     server: {
-      baseDir: 'app'
+      baseDir: 'app',
     },
-    notify: false
+    notify: false,
   });
 });
 
-gulp.task('watch',['browser-sync', 'sass'], function() {
+gulp.task('watch', ['browser-sync', 'sass'], () => {
   gulp.watch('app/scss/**/*.scss', ['sass']);
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/scripts/**/*.js', browserSync.reload);
 });
 
-gulp.task('clean', function () {
-  return del.sync('dist');
-});
-gulp.task('clear', function () {
-  return cache.clearAll();
-});
-gulp.task('img', function () {
-  return gulp.src('app/images/**/*')
-    .pipe(cache(imagemin({
-      interlaced: true,
-      progressive: true,
-      svgoPlugins: [{
-        removeViewbox: false
-      }],
-      use: [pngquant()]
-    })))
-    .pipe(gulp.dest('dist/images'))
-});
+gulp.task('clean', () => del.sync('dist'));
+gulp.task('clear', () => cache.clearAll());
+gulp.task('img', () => gulp.src('app/images/**/*')
+  .pipe(cache(imagemin({
+    interlaced: true,
+    progressive: true,
+    svgoPlugins: [{
+      removeViewbox: false,
+    }],
+    use: [pngquant()],
+  })))
+  .pipe(gulp.dest('dist/images')));
 
-gulp.task('build', ['clean', 'img' ,'sass',], function () {
-  var buildCss = gulp.src('app/css/*.css')
+gulp.task('build', ['clean', 'img', 'sass'], () => {
+  const buildCss = gulp.src('app/css/*.css')
     .pipe(gulp.dest('dist/css'));
-  var buildFonts = gulp.src('app/fonts/**/*')
+  const buildFonts = gulp.src('app/fonts/**/*')
     .pipe(gulp.dest('dist/fonts'));
-  var buildImages = gulp.src('app/images/**/*')
+  const buildImages = gulp.src('app/images/**/*')
     .pipe(gulp.dest('dist/images'));
-  var buildHtml = gulp.src('app/*.html')
+  const buildHtml = gulp.src('app/*.html')
     .pipe(gulp.dest('dist'));
-  var buildJs = gulp.src('app/scripts/**/*')
-    .pipe(gulp.dest('dist/scripts'))
+  const buildJs = gulp.src('app/scripts/**/*')
+    .pipe(gulp.dest('dist/scripts'));
 });
 
 gulp.task('default', ['watch']);
